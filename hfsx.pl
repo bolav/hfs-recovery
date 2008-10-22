b2c0a60b50333219d720b0e6024a5c28ca13fb0f
bolavs <bolavs@c327dfc8-9e99-11dd-a482-49de035e4cd6>
2008-10-22 09:21:06 +0000

    added license

--- hfsx.pl
+++ hfsx.pl
@@ -1,5 +1,22 @@
 #!/usr/bin/perl
 
+#    This file is part of hfs-recovery.
+#
+#    hfs-recovery is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    hfs-recovery is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with hfs-recovery.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    Bjorn-Olav Strand <bo@startsiden.no>
+
 use strict;
 use IO::File;
 use Filesystem::HFS::BTNodeDescriptor;
@@ -20,6 +37,8 @@ my $blockSize = 4096;
 my $fh = new IO::File;
 $fh->open($image) || die "Couldnt open";
 
+# No support for more that 8 extents
+# - only applies to large files!
 # Todo: read threads?
 # Support large files (runs out of memory)
 # Support working disks? 
@@ -349,8 +368,33 @@ sub cp_id
   print "Writing from $oldf to $fn\n";
   my $of;
   open($of,">$fn");
-  print $of &get_id($cnid);
+  &save_id($cnid,$of);
   close($of);
+}
+
+sub save_id
+{
+  my $cnid = shift;
+  my $sfh = shift;
+  my $read = 0;
+
+  my $df = $rec_cnid{$cnid}->dataFork();
+  print "Size: ",$df->logicalSize(),"\n";
+  my $size = $df->logicalSize();
+  my $buf;
+  
+  my $eds = $df->extents()->extentDescriptors();
+  foreach my $ed (@{$eds}) {
+    if ($ed->blockCount()>0) {
+      print "Reading: ",$ed->blockCount()," from ",$ed->startBlock(),"\n";
+      $fh->seek($ed->startBlock()*$blockSize,0);
+      my $r = $fh->read($buf,$blockSize*$ed->blockCount());
+      if (($read + $r) > $size) {
+        $buf = substr($buf,0,$size-$read);
+      }
+      print $sfh $buf;
+    }
+  }
 }
 
 # This will crash for large files.
